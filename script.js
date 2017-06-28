70b078d43beedb3b5b21e2f0e3caff5b0d7363bb
Arthur Guo <arthur.guo@uwaterloo.ca>
2017-06-28 19:59:32 -0400

    Change hover action to tooltip

--- script.js
+++ script.js
@@ -10,37 +10,69 @@ function process_query(input_str){
     console.log(input_str);
 }
 
+function showToolbar(d){
+    var copiedNode = jQuery.extend({}, d);
+    delete copiedNode.x
+    delete copiedNode.y
+    delete copiedNode.vy
+    delete copiedNode.vx
+    div.transition()        
+        .duration(200)      
+        .style("opacity", .9);      
+    div.html(JSON.stringify(copiedNode)+"<br/>")  
+        .style("left", (d3.event.pageX) + "px")     
+        .style("top", (d3.event.pageY - 28) + "px");    
+}
 
-//Helper function to update info at the bottom of the graph
-function updateHoverInfo(info){
-    unhoverItem();
-    var $info = $("#hover-info");
-    var $template = $("#hover-pair-template");
-    Object.keys(info).forEach(function(attribute){
-        var $clone = $template.clone();
-        $clone.attr("id", "").attr("class", "hover-pair");
-        $clone.find(".hover-key").text(attribute)
-            $clone.find(".hover-value").text(info[attribute]);
-        $clone.appendTo($info);
-    });
+/* D3 tooltip */
+var stickyNode; //Node to determine where the toolbar is currently placed
+
+function hideToolbar(d){
+    div.transition()        
+        .duration(500)      
+        .style("opacity", 0);   
 }
 
 //Handling hover nodes
 function hoverNode(d){
-    console.log(d);
-    updateHoverInfo(d);
+    showToolbar(d);
+    if(d!==stickyNode){
+        stickyNode = null;
+    }
+}
+
+function unhoverNode(d){
+    if(!stickyNode){
+        hideToolbar();
+    }
+}
+
+//Handling hover nodes
+function clickNode(d){
+    if(stickyNode){
+        hideToolbar();
+    }
+    else{
+        showToolbar(d);
+        stickyNode = d;
+    }
 }
 
 //Handling hover Edges
-function hoverEdge(d){
-    unhoverItem();
-    updateHoverInfo(info);
+function hoverLink(d){
+    showToolbar(d);
 }
 
 function unhoverItem(){
     var $info = $("#hover-info");
     $info.find(".hover-pair").remove()
 }
+
+
+// Define the div for the tooltip
+var div = d3.select("body").append("div")    
+.attr("class", "tooltip")                
+.style("opacity", 0);
 
 var svg = d3.select("svg"),
     width = +svg.attr("width"),
@@ -49,74 +81,78 @@ var svg = d3.select("svg"),
 var color = d3.scaleOrdinal(d3.schemeCategory20);
 
 var simulation = d3.forceSimulation()
-  .force("link", d3.forceLink().id(function(d) { return d.id; }))
-  .force("charge", d3.forceManyBody())
-  .force("center", d3.forceCenter(width / 2, height / 2));
+    .force("link", d3.forceLink().id(function(d) { return d.id; }))
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter(width / 2, height / 2));
 
-  d3.json("miserables.json", function(error, graph) {
-    if (error) throw error;
+    d3.json("miserables.json", function(error, graph) {
+        if (error) throw error;
 
-    var link = svg.append("g")
-      .attr("class", "links")
-      .selectAll("line")
-      .data(graph.links)
-      .enter().append("line")
-      .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+        var link = svg.append("g")
+            .attr("class", "links")
+            .selectAll("line")
+            .data(graph.links)
+            .enter().append("line")
+            .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
 
-    link.on("click", hoverNode);
+        link.on("click", hoverLink);
+        link.on("mouseover", hoverLink);
 
-    var node = svg.append("g")
-      .attr("class", "nodes")
-      .selectAll("circle")
-      .data(graph.nodes)
-      .enter().append("circle")
-      .attr("r", 10)
-      .attr("fill", function(d) { return color(d.group); })
-      .call(d3.drag()
-          .on("start", dragstarted)
-          .on("drag", dragged)
-          .on("end", dragended));
+        var node = svg.append("g")
+            .attr("class", "nodes")
+            .selectAll("circle")
+            .data(graph.nodes)
+            .enter().append("circle")
+            .attr("r", 10)
+            .attr("fill", function(d) { return color(d.group); })
+            .call(d3.drag()
+                    .on("start", dragstarted)
+                    .on("drag", dragged)
+                    .on("end", dragended));
 
-    node.append("title")
-      .text(function(d) { return d.id; });
+        node.append("title")
+            .text(function(d) { return d.id; });
 
-    node.on("click", hoverNode);
+        node.on("click", clickNode);
 
-    simulation
-      .nodes(graph.nodes)
-      .on("tick", ticked);
+        node.on("mouseover", hoverNode)
+            .on("mouseout", unhoverNode);
 
-    simulation.force("link")
-      .links(graph.links);
+        simulation
+            .nodes(graph.nodes)
+            .on("tick", ticked);
 
-    function ticked() {
-      link
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+        simulation.force("link")
+            .links(graph.links);
 
-      node
-        .attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-    }
-  });
+        function ticked() {
+            link
+                .attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; });
+
+            node
+                .attr("cx", function(d) { return d.x; })
+                .attr("cy", function(d) { return d.y; });
+        }
+    });
 
 function dragstarted(d) {
-  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-  d.fx = d.x;
-  d.fy = d.y;
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
 }
 
 function dragged(d) {
-  d.fx = d3.event.x;
-  d.fy = d3.event.y;
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
 }
 
 function dragended(d) {
-  if (!d3.event.active) simulation.alphaTarget(0);
-  d.fx = null;
-  d.fy = null;
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
 }
 
 
